4a3c8461af375e2d133dfac9924e5aa98c2489a4
Jeremy Smith <jmsmith57@hotmail.com>
2017-07-10 15:49:55 +0100

    skeleton is forming up

--- app.js
+++ app.js
@@ -8,6 +8,15 @@ angular
       .state("home", {
         url: "/",
         templateUrl: "templates/home.html"
-      });      
+      })
+      .state("about", {
+        url: "/about",
+        templateUrl: "templates/about.html"
+      })
+      .state("contact", {
+        url: "/contact",
+        templateUrl: "templates/contact.html"
+      });   
+         
     $urlRouterProvider.otherwise("/")
   }
