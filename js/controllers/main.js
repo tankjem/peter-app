093f73a430e5e5926fdc513b27276c2641b5d004
Jeremy Smith <jmsmith57@hotmail.com>
2017-08-01 16:57:35 +0100

    First draft done

--- js/controllers/main.js
+++ js/controllers/main.js
@@ -8,8 +8,9 @@ angular
     $scope.isCollapsed = false;
     $scope.isCollapsedHorizontal = false;
 
-    $scope.firstVid = 'https://www.youtube.com/embed/gEt4u1C7F-k?ecver=1';
+    // $scope.firstVid = 'https://www.youtube.com/embed/gEt4u1C7F-k?ecver=1';
     $scope.secondVid = 'https://www.youtube.com/embed/ZZEc7B2rALM?ecver=1';
     $scope.thirdVid = 'https://www.youtube.com/embed/ff7nudbAKTc?ecver=1';
     $scope.fourthVid = 'https://www.youtube.com/embed/wsJ06bW4nsc?ecver=1';
+    $scope.fifthVid = 'https://www.youtube.com/embed/luiFz9zeRTI?ecver=1';
   }
